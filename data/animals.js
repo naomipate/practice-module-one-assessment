aee2253965bd592756bd1304e824ed0ec3753bdf
Anthony Pinzone <anthonypinzone@pursuit.org>
2023-01-31 17:50:54 -0500

    Split the data into individual files and added books.

--- data/animals.js
+++ data/animals.js
@@ -1,41 +1,3 @@
-// Array of People
-let people = [
-  {
-    name: "John",
-    age: 25,
-    hobbies: ["reading", "hiking", "traveling"],
-    address: {
-      street: "123 Main St",
-      city: "Anytown",
-      state: "CA",
-      zip: "98765",
-    },
-  },
-  {
-    name: "Jane",
-    age: 32,
-    hobbies: ["cooking", "traveling"],
-    address: {
-      street: "456 Elm St",
-      city: "Smallville",
-      state: "NY",
-      zip: "12345",
-    },
-  },
-  {
-    name: "Bob",
-    age: 18,
-    hobbies: ["gaming", "hiking", "traveling"],
-    address: {
-      street: "789 Oak St",
-      city: "Bigcity",
-      state: "TX",
-      zip: "67890",
-    },
-  },
-];
-
-// Array of Animals
 const animals = [
   {
     name: "Lion",
@@ -106,6 +68,5 @@ const animals = [
 ];
 
 module.exports = {
-  people,
   animals,
 };
---
+++ data/books.js
@@ -0,0 +1,76 @@
+const books = [
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    publicationYear: 1997,
+    genre: "Fantasy",
+    isbn: "9780747538493",
+  },
+  {
+    title: "The Shining",
+    author: "Stephen King",
+    publicationYear: 1977,
+    genre: "Horror",
+    isbn: "9780307743657",
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    publicationYear: 1813,
+    genre: "Romance",
+    isbn: "9780141439587",
+  },
+  {
+    title: "The Adventures of Tom Sawyer",
+    author: "Mark Twain",
+    publicationYear: 1876,
+    genre: "Adventure",
+    isbn: "9780142437209",
+  },
+  {
+    title: "War and Peace",
+    author: "Leo Tolstoy",
+    publicationYear: 1869,
+    genre: "Historical Fiction",
+    isbn: "9780143035008",
+  },
+  {
+    title: "The Handmaid's Tale",
+    author: "Margaret Atwood",
+    publicationYear: 1985,
+    genre: "Dystopian Fiction",
+    isbn: "9780385490818",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    publicationYear: 1949,
+    genre: "Dystopian Fiction",
+    isbn: "9780451524935",
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    publicationYear: 1960,
+    genre: "Literary Fiction",
+    isbn: "9780061120084",
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    publicationYear: 1925,
+    genre: "Literary Fiction",
+    isbn: "9780743273565",
+  },
+  {
+    title: "A Farewell to Arms",
+    author: "Ernest Hemingway",
+    publicationYear: 1929,
+    genre: "Literary Fiction",
+    isbn: "9780684801469",
+  },
+];
+
+module.exports = {
+  books,
+};
